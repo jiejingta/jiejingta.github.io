168b23400c34fff9d9d64a42b26c03ba49c6c694
ppmei <906612949@qq.com>
2020-01-29 21:23:59 +0800

    add spectrum for palette
    添加色轮和调色盘，用的是sprctrum插件，很奈斯

---
+++ spectrum.js
@@ -0,0 +1,29 @@
+$("#full").spectrum({
+    color: "#ECC",
+    flat: true,
+    showInput: true,
+    className: "full-spectrum",
+    showInitial: true,
+    showPalette: true,
+    showSelectionPalette: true,
+    maxPaletteSize: 10,
+    preferredFormat: "hex",
+    localStorageKey: "spectrum.demo",
+    move: function (color) {
+        
+    },
+    show: function () {
+    
+    },
+    beforeShow: function () {
+    
+    },
+    hide: function () {
+    
+    },
+    change: function() {
+        
+    },
+    palette: [
+    ]
+});
